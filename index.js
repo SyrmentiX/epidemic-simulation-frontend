d24cbbac500e3f73a7e759822563b6bd7f15edd2
Syrm <anton.bayka@gmail.com>
2020-04-18 21:03:28 +1000

    add main page

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 Vue.component('mapbox',{
-	template: '<div v-show="true" id="map" style="width: 80%; height: 100%;"></div>'
+	template: '<div v-show="true" id="map" class="mapbox"></div>'
 })
 
-Vue.component('menu', {
-	template: '<div v-show="false">\
-		\
+Vue.component('settings-menu', {
+	template: '<div v-show="true" class="settings-menu">\
+			<div class="title">\
+			<h1>EPIDEMICSM</h1>\
+			</div>\
+			<div class="settings-column">\
+				<p>todo Timeline</p>\
+			</div>\
+			<div class="settings-column">\
+				<button class="simulation-button">Launch</button>\
+				<button class="simulation-button">Stop</button>\
+			</div>\
+			<div class="settings-column">\
+				<p>todo Presets</p>\
+			</div>\
 		</div>'
 })
 
