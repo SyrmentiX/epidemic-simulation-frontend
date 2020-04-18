82f68617fd2bb5feb02e8d650ac686716244d866
Syrm <anton.bayka@gmail.com>
2020-04-18 21:50:39 +1000

    add circle draw

--- map.js
+++ map.js
@@ -1,5 +1,60 @@
 mapboxgl.accessToken = 'pk.eyJ1Ijoic3lybSIsImEiOiJjazk1ZGJqZnEwNDJlM21tcHZxbnRwbW1tIn0.2GX6n3BUAz-c4vqDKb6dpw';
+
 var map = new mapboxgl.Map({
 	container: 'map',
 	style: 'mapbox://styles/mapbox/streets-v11'
+});
+
+var createGeoJSONCircle = function(center, radiusInKm, points) {
+    if(!points) points = 64;
+
+    var coords = {
+        latitude: center[1],
+        longitude: center[0]
+    };
+
+    var km = radiusInKm;
+
+    var ret = [];
+    var distanceX = km/(111.320*Math.cos(coords.latitude*Math.PI/180));
+    var distanceY = km/110.574;
+
+    var theta, x, y;
+    for(var i=0; i<points; i++) {
+        theta = (i/points)*(2*Math.PI);
+        x = distanceX*Math.cos(theta);
+        y = distanceY*Math.sin(theta);
+
+        ret.push([coords.longitude+x, coords.latitude+y]);
+    }
+    ret.push(ret[0]);
+
+    return {
+        "type": "geojson",
+        "data": {
+            "type": "FeatureCollection",
+            "features": [{
+                "type": "Feature",
+                "geometry": {
+                    "type": "Polygon",
+                    "coordinates": [ret]
+                }
+            }]
+        }
+    };
+};
+
+map.on('load', function() {
+	map.addSource("polygon", createGeoJSONCircle([0, 0], 1000));
+
+	map.addLayer({
+	    "id": "polygon",
+    	"type": "fill",
+    	"source": "polygon",
+    	"layout": {},
+    	"paint": {
+	        "fill-color": "blue",
+    	    "fill-opacity": 0.6
+    	}
+	});
 });
