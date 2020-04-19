79c106ab7e9df4ab41e519394ef18b727799559a
Syrm <anton.bayka@gmail.com>
2020-04-19 11:21:07 +1000

    Add country highlight

--- map.js
+++ map.js
@@ -1,91 +1,43 @@
 mapboxgl.accessToken = 'pk.eyJ1Ijoic3lybSIsImEiOiJjazk1ZGJqZnEwNDJlM21tcHZxbnRwbW1tIn0.2GX6n3BUAz-c4vqDKb6dpw';
 
+var countryInformation;
+
 var map = new mapboxgl.Map({
 	container: 'map',
 	style: 'mapbox://styles/mapbox/streets-v11'
 });
 
-var curclesId = 0;
-
-var markCountry = function(countryName) {
-	countryName
-	var json = "https://api.mapbox.com/geocoding/v5/mapbox.places/"+countryName+".json?access_token=pk.eyJ1Ijoic3lybSIsImEiOiJjazk1ZGJqZnEwNDJlM21tcHZxbnRwbW1tIn0.2GX6n3BUAz-c4vqDKb6dpw"
-	var request = new XMLHttpRequest();
-	request.open('GET', json)
-	request.responseType = 'json';
-	request.send();
-	request.onload = function() {
-		var coordinates = request.response;
-	  	for (var i = coordinates.features.length - 1; i >= 0; i--) {
-	  		if (coordinates.features[i].place_type[0] == "country") {
-	  			writeCircle(coordinates.features[i]);
-	  			return coordinates.features[i];
-	  		}
-	  	}
-	  	return null;
-	}
-}
-
-var writeCircle = function(country) {
-	console.log(country);
-	if (country != null) {
-		map.addSource("polygon"+curclesId, createGeoJSONCircle(country.center, 100));
-
-		map.addLayer({
-		    "id": "polygon"+curclesId,
-	    	"type": "fill",
-    		"source": "polygon"+curclesId,
-    		"layout": {},
-    		"paint": {
-	        	"fill-color": "blue",
-    	    	"fill-opacity": 0.6
-    		}
+var drawLayers = function(coronaJson, countriesJson) {
+	for (var i = countriesJson.features.length - 1; i >= 0; i--) {
+		map.addSource('country'+countriesJson.features[i].properties.ADMIN, {
+			'type': 'geojson',
+			'data': countriesJson.features[i]
 		});
-		++curclesId;
+
+	 	map.addLayer({
+			'id': 'layer'+countriesJson.features[i].properties.ADMIN,
+			'type': 'fill',
+			'source': 'countries'+countriesJson.features[i].properties.ADMIN,
+			'paint': {
+				'fill-color': 'red',
+				'fill-opacity': '0.6'
+				'fill-outline-color': 'rgba(200, 100, 240, 1)'					
+	 	 	}
+		});
 	}
 }
 
-var createGeoJSONCircle = function(center, radiusInKm, points) {
-    if (!points) points = 64;
-
-    var coords = {
-        latitude: center[1],
-        longitude: center[0]
-    };
-
-    var km = radiusInKm;
-
-    var ret = [];
-    var distanceX = km/(111.320*Math.cos(coords.latitude*Math.PI/180));
-    var distanceY = km/110.574;
-
-    var theta, x, y;
-    for(var i=0; i<points; i++) {
-        theta = (i/points)*(2*Math.PI);
-        x = distanceX*Math.cos(theta);
-        y = distanceY*Math.sin(theta);
-
-        ret.push([coords.longitude+x, coords.latitude+y]);
-    }
-    ret.push(ret[0]);
-
-    return {
-        "type": "geojson",
-        "data": {
-            "type": "FeatureCollection",
-            "features": [{
-                "type": "Feature",
-                "geometry": {
-                    "type": "Polygon",
-                    "coordinates": [ret]
-                }
-            }]
-        }
-    };
-};
+var onUpdate = function() {
+	$.getJSON("http://104.248.59.99:8080/today", function(infoJson) {
+		console.log(infoJson);
+		drawLayers(infoJson, countryInformation);	
+	});
+}
 
 map.on('load', function() {
-	markCountry("Russia Federation");
-	markCountry("USA");
-	markCountry("United Kingdom");
+	$.getJSON("http://104.248.59.99/ne_110m_admin_0_countries.geojson", function(countriesJson) {
+		console.log(countriesJson);
+		countryInformation = countriesJson;
+		onUpdate();
+	});
 });
