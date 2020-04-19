ed26ff16aff91d9900f00bc84a3cdb8a2f1f5325
Syrm <anton.bayka@gmail.com>
2020-04-19 17:15:14 +1000

    add infected popup

--- map.js
+++ map.js
@@ -7,24 +7,63 @@ var map = new mapboxgl.Map({
 	style: 'mapbox://styles/mapbox/streets-v11'
 });
 
-var drawLayers = function(coronaJson, countriesJson) {
-	for (var i = countriesJson.features.length - 1; i >= 0; i--) {
-		map.addSource('country'+countriesJson.features[i].properties.ADMIN, {
-			'type': 'geojson',
-			'data': countriesJson.features[i]
-		});
+var hoveredStateId = null;
 
-	 	map.addLayer({
-			'id': 'layer'+countriesJson.features[i].properties.ADMIN,
-			'type': 'fill',
-			'source': 'countries'+countriesJson.features[i].properties.ADMIN,
-			'paint': {
-				'fill-color': 'red',
-				'fill-opacity': '0.6'
-				'fill-outline-color': 'rgba(200, 100, 240, 1)'					
-	 	 	}
-		});
-	}
+var popup = new mapboxgl.Popup({
+	closeButton: false,
+	closeOnClick: false
+});
+
+var drawLayers = function(coronaJson, countriesJson) {
+	map.addSource('countries', {
+		'type': 'geojson',
+		'data': countriesJson
+	});
+
+	map.addLayer({
+		'id': 'countries',
+		'type': 'fill',
+		'source': 'countries',
+		'paint': {
+			'fill-color': 'red',
+			'fill-opacity': [
+				'case',
+				['boolean', ['feature-state', 'hover'], false],
+				1,
+				0.5
+			],
+			'fill-outline-color': 'rgba(200, 100, 240, 1)'					
+	 	 }
+	});
+
+	map.on('mousemove', 'countries', function(e) {
+		map.getCanvas().style.cursor = 'pointer';
+		if (e.features.length > 0) {
+			if (hoveredStateId) {
+				map.getCanvas().style.cursor = '';
+				popup.remove();
+			}
+
+			var coordinates = e.features[0].geometry.coordinates.slice();
+			while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+				coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+			}
+			var postalId = coronaJson.data[0].countries[e.features[0].properties.WB_A2];
+			if (postalId != null) {
+				hoveredStateId = e.features[0].id;
+				popup.setLngLat(e.lngLat).setHTML(postalId.infected).addTo(map);
+			}
+		}
+	});
+
+	map.on('mouseleave', 'countries', function() {
+		if (hoveredStateId) {
+			map.getCanvas().style.cursor = '';
+			popup.remove();
+		}
+		hoveredStateId = null;
+		
+	});
 }
 
 var onUpdate = function() {
@@ -35,7 +74,7 @@ var onUpdate = function() {
 }
 
 map.on('load', function() {
-	$.getJSON("http://104.248.59.99/ne_110m_admin_0_countries.geojson", function(countriesJson) {
+	$.getJSON("http://104.248.59.99/ne_110m_admin_0_countries_fixed.geojson", function(countriesJson) {
 		console.log(countriesJson);
 		countryInformation = countriesJson;
 		onUpdate();
